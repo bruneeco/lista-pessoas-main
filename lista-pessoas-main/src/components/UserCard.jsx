81d52500707356e456211797f50def5ac9d5b0a8
bruneecoo <bruno12zxz@gmail.com>
2025-06-06 12:01:55 -0300

    colocando tudo nos lugares

---
+++ lista-pessoas-main/src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+import './UserCard.css';
+
+function UserCard({ user }) {
+    return (
+        <div className="user-card">
+            <img src={user.avatar} alt={`${user.name}'s avatar`} className="user-avatar" />
+            <h3>{user.firstName} {user.lastName}</h3>
+            <p>{user.email}</p>
+            <small>{user.address}</small>
+            </div>
+    );
+}
+export default UserCard;
